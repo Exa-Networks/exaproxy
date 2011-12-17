abe467cfa49ecd4d4a8ed8e4f23f21cf11462884
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2011-12-17 13:02:28 +0000

    conf change

--- lib/exaproxy/configuration.py
+++ lib/exaproxy/configuration.py
@@ -16,7 +16,7 @@ import syslog
 from .util.logger import logger
 from .util.version import version
 
-_enabled = ('1','yes','Yes','YES','on','ON')
+_enabled = ('1','yes','on','enable')
 _all = os.environ.get('DEBUG_ALL','0') != '0'
 
 _priorities = {
@@ -44,27 +44,27 @@ class log:
 class _Configuration (object):
 	_instance = None
 	
-	PID = os.environ.get('PID','')
-	USER = os.environ.get('USER','nobody')
+	PID       = os.environ.get('PID','')
+	USER      = os.environ.get('USER','nobody')
 	DAEMONIZE = os.environ.get('DAEMONIZE','0') not in ['','1','yes','Yes','YES']
-	VERSION = version
-	PROGRAM = dict(zip(range(len(sys.argv)),sys.argv)).get(2,'') # I must like perl :)
-	SPEED = 2 # 0.01
-	CONNECT = os.environ.get('CONNECT','1') in _enabled
-	PROFILE    = os.environ.get('PROFILE','0')
+	VERSION   = version
+	PROGRAM   = dict(zip(range(len(sys.argv)),sys.argv)).get(2,'') # I must like perl :)
+	SPEED     = 2 # 0.01
+	CONNECT   = os.environ.get('CONNECT','1').lower() in _enabled
+	PROFILE   = os.environ.get('PROFILE','0')
 
 	def __init__ (self):
 		if os.environ.get('SYSLOG',None):
 			logger.syslog()
 		logger.level = _priorities.get(os.environ.get('LOG',None),syslog.LOG_DEBUG if _all else syslog.LOG_ERR)
-		logger.debug_supervisor = os.environ.get('DEBUG_SUPERVISOR','1') in _enabled or _all
-		logger.debug_daemon = os.environ.get('DEBUG_DAEMON','1') in _enabled or _all
-		logger.debug_server = os.environ.get('DEBUG_SERVER','1') in _enabled or _all
-		logger.debug_client = os.environ.get('DEBUG_CLIENT','1') in _enabled or _all
-		logger.debug_manager = os.environ.get('DEBUG_MANAGER','1') in _enabled or _all
-		logger.debug_worker = os.environ.get('DEBUG_WORKER','1') in _enabled or _all
-		logger.debug_download = os.environ.get('DEBUG_DOWNLOAD','1') in _enabled or _all
-		logger.debug_http = os.environ.get('DEBUG_HTTP','1') in _enabled or _all
+		logger.debug_supervisor = os.environ.get('DEBUG_SUPERVISOR','1').lower() in _enabled or _all
+		logger.debug_daemon = os.environ.get('DEBUG_DAEMON','1').lower() in _enabled or _all
+		logger.debug_server = os.environ.get('DEBUG_SERVER','1').lower() in _enabled or _all
+		logger.debug_client = os.environ.get('DEBUG_CLIENT','1').lower() in _enabled or _all
+		logger.debug_manager = os.environ.get('DEBUG_MANAGER','1').lower() in _enabled or _all
+		logger.debug_worker = os.environ.get('DEBUG_WORKER','1').lower() in _enabled or _all
+		logger.debug_download = os.environ.get('DEBUG_DOWNLOAD','1').lower() in _enabled or _all
+		logger.debug_http = os.environ.get('DEBUG_HTTP','1').lower() in _enabled or _all
 
 def Configuration ():
 	if _Configuration._instance:
