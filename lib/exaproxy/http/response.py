8a9fb351028a0179a7bc4b3eded5d4db11dceb62
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-01-14 19:23:59 +0000

    re-add _http as comment

--- lib/exaproxy/http/response.py
+++ lib/exaproxy/http/response.py
@@ -7,6 +7,23 @@ Created by Thomas Mangin on 2011-12-02.
 Copyright (c) 2011 Exa Networks. All rights reserved.
 """
 
+#import sys
+#from exaproxy.configuration import configuration
+#from exaproxy.util.version import version
+#
+#def _http (code,message):
+#        return """\
+#HTTP/1.1 %d OK
+#Date: Fri, 02 Dec 2011 09:29:44 GMT
+#Server: exaproxy/%s (%s)
+#Content-Length: %d
+#Connection: close
+#Content-Type: text/html
+#Cache-control: private
+#Pragma: no-cache
+#
+#%s""" % (code,str(version),sys.platform,len(message),message)
+
 import os
 
 from exaproxy.util.logger import logger
