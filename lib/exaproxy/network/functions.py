f4a15519d02181b91fa72fd3d6d05cfd6b127785
David Farrar <david.farrar@exa-networks.co.uk>
2012-01-25 14:08:13 +0000

    connection to server is nonblocking

--- lib/exaproxy/network/functions.py
+++ lib/exaproxy/network/functions.py
@@ -12,6 +12,12 @@ import errno
 
 from exaproxy.util.logger import logger
 
+errno_block = set((
+        errno.EAGAIN, errno.EWOULDBLOCK,
+        errno.EINTR,
+))
+
+
 def _ipv4(address):
 	try:
 		socket.inet_pton(socket.AF_INET, address)
@@ -73,10 +79,16 @@ def connect (ip,port):
 	except socket.error,e:
 		return None
 	try:
-		##s.setblocking(0)
+		s.setblocking(0)
 		s.connect((ip, port))
-		return s
 	except socket.error,e:
-		return None
-#		if e.errno in (errno.EINPROGRESS,):
-#			return None
+		if e.errno == errno.EINPROGRESS:
+			pass
+
+		elif e.errno in errno_block:
+			pass
+
+		else:
+			s = None
+
+	return s
