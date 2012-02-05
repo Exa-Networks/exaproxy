7223569a96829630f5ed37a53fb741b799568be7
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-02-05 18:50:05 +0000

    explain the configuration options

--- lib/exaproxy/configuration.py
+++ lib/exaproxy/configuration.py
@@ -44,24 +44,24 @@ class log:
 class _Configuration (object):
 	_instance = None
 
-	HOST      = os.environ.get('HOST','127.0.0.1')
-	PORT      = int(os.environ.get('PORT')) if os.environ.get('PORT','').isdigit() else 31280
-	TIMEOUT   = int(os.environ.get('TIMEOUT')) if os.environ.get('TIMEOUT','').isdigit() else 31280
-	BACKLOG   = int(os.environ.get('BACKLOG')) if os.environ.get('BACKLOG','').isdigit() else 200
-	SPEED     = 2 # 0.01
+	HOST      = os.environ.get('HOST','127.0.0.1') # tcp host for proxy
+	PORT      = int(os.environ.get('PORT')) if os.environ.get('PORT','').isdigit() else 31280 # tcp port for proxy
+	TIMEOUT   = int(os.environ.get('TIMEOUT')) if os.environ.get('TIMEOUT','').isdigit() else 31280 # tcp connection timeout
+	BACKLOG   = int(os.environ.get('BACKLOG')) if os.environ.get('BACKLOG','').isdigit() else 200 # tcp connection backlog
+	SPEED     = 2 # select waiting timeout
 
-	PID       = os.environ.get('PID','')
-	USER      = os.environ.get('USER','nobody')
-	DAEMONIZE = os.environ.get('DAEMONIZE','0').lower() in _enabled
+	PID       = os.environ.get('PID','') # where to save the PID if we do
+	USER      = os.environ.get('USER','nobody') # whatuser right to use if we are root
+	DAEMONIZE = os.environ.get('DAEMONIZE','0').lower() in _enabled # should the program become a daemon
 
-	VERSION   = version
-	PROGRAM   = (sys.argv + ['', ''])[1] # I don't like perl :)
+	VERSION   = version # version of the program
+	PROGRAM   = (sys.argv + ['', ''])[1] # program name used
 
-	CONNECT   = os.environ.get('CONNECT','1').lower() in _enabled
-	PROFILE   = os.environ.get('PROFILE','0')
-	RESOLV    = os.environ.get('RESOLV', '/etc/resolv.conf')
-	MIN_WORK  = int(os.environ.get('MIN_WORKERS')) if os.environ.get('MIN_WORKERS','').isdigit() else 5
-	MAX_WORK  = int(os.environ.get('MAX_WORKERS')) if os.environ.get('MAX_WORKERS','').isdigit() else 25
+	CONNECT   = os.environ.get('CONNECT','1').lower() in _enabled # do we allow the CONNECT method
+	PROFILE   = os.environ.get('PROFILE','0') # Turn on profiling and if a file (ie not 0/1) save the result there
+	RESOLV    = os.environ.get('RESOLV', '/etc/resolv.conf') # The resolver file to use
+	MIN_WORK  = int(os.environ.get('MIN_WORKERS')) if os.environ.get('MIN_WORKERS','').isdigit() else 5 # how many worker thread will we always have
+	MAX_WORK  = int(os.environ.get('MAX_WORKERS')) if os.environ.get('MAX_WORKERS','').isdigit() else 25 # how many worker thread is our maximum
 
 	_location = os.path.normpath(sys.argv[0]) if sys.argv[0].startswith('/') else os.path.normpath(os.path.join(cwd,sys.argv[0]))
 	_paths = (
