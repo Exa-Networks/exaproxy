6550695b23dfecbc17adb1db029fbf340be76961
David Farrar <david.farrar@exa-networks.co.uk>
2012-02-20 18:15:17 +0000

    Further cleanup of DNS library - move files to a more sensible location. Not yet working again

---
+++ lib/exaproxy/dns/dnstype.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+querytype.py
+
+Created by David Farrar on 2012-02-08.
+Copyright (c) 2011 Exa Networks. All rights reserved.
+"""
+
+import os
+import convert
+
+class DNSType:
+	def __str__(self):
+		return str(self.question)
+
+class DNSQueryType(DNSType):
+	def __init__(self, querytype, question, queryclass=1):
+		self.queryclass = queryclass
+		self.querytype = querytype
+		self.question = question
+
+	def __str__(self):
+		return "Query of type %s for %s" % (self.querytype, self.question)
+
+class DNSResourceType(DNSType):
+	def __init__(self, querytype, question, response, ttl, queryclass=1):
+		self.queryclass = queryclass
+		self.querytype = querytype
+		self.question = question
+		self.response = response
+		self.ttl = ttl
+
+	def __str__(self):
+		return "Resource of type %s for %s: %s" % (self.querytype, self.question, self.response)
+
+
+conversion = {
+	'ipv4' : (convert.ipv4_to_dns, convert.dns_to_ipv4),
+	'ipv6' : (convert.ipv6_to_dns, convert.dns_to_ipv6),
+	'string' : (convert.string_to_dns, convert.dns_to_string),
+	'unimplemented' : (None, None),
+}
+
+
+
+class DNSTypeFactory:
+	CLASS = 1   # Internet
+
+	def createQuery(self, name, question):
+		return DNSQueryType(name, question)
+
+	def createResource(name, question, response, ttl):
+		return DNSResourceType(name, question, response, ttl)
+
+
+
+class DNSTypeCodec:
+	CLASS = 1   # Internet
+
+	def __init__(self, base):
+		self.byname = {}
+		self.byvalue = {}
+
+		filename = os.path.join(base, 'dns', 'types')
+		self.parseConfiguration(filename)
+
+	def parseConfiguration(self, filename):
+		try:
+			with open(filename) as fd:
+				for line in fd:
+					line = line.strip().split('#', 1)[0]
+					if line.startswith('#'):
+						continue
+
+					name, value, querytype = line.split()
+					value = int(value)
+
+					if name in self.byname:
+						raise ValueError, 'Configuration file defines record of type %s more than once' % name
+
+					if value in self.byvalue:
+						raise ValueError, 'Configuration file defines record with value %s more than once' % value
+
+					if querytype not in conversion:
+						raise ValueError, 'Configuration file uses undefined type: %s' % querytype
+
+					encoder, decoder = conversion[querytype]
+
+					self.byname[name] = value, encoder
+					self.byvalue[value] = name, decoder
+
+		except (IndexError, ValueError):
+			raise RuntimeError, 'Corrupt DNS type definition'
+		except TypeError:
+			raise RuntimeError, 'Corrupt DNS type definition'
+		except IOError:
+			raise RuntimeError, 'Cannot read DNS type definition file: %s' % filename
+
+	def decodeQuery(self, value, question, data_s=''):
+		name, decoder = self.byvalue.get(value, (None, None))
+		return DNSQueryType(name, question)
+
+	def encodeQuery(self, question, data_s=''):
+		value, encoder = self.byname.get(question.querytype, (None, None))
+		return value, question if value is not None else None
+
+	def decodeResource(self, value, question, response, ttl, data_s=''):
+		name, decoder = self.byvalue.get(value, (None, None))
+		if name is not None:
+			decoded = decoder(response, data_s)
+		else:
+			decoded = None
+			
+		return DNSResourceType(name, question, decoded, ttl)
+
+	def encodeResource(self, resource, data_s=''):
+		value, encoder = self.byvalue.get(name, (None, None))
+		if value is not None:
+			encoded = encoder(resource.response, data_s)
+		else:
+			encoded = None
+
+		return value, resource.question, encoded, resource.ttl
--- lib/exaproxy/dns/factory/packet/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-__init__.py
-
-Created by David Farrar on 2012-02-08.
-Copyright (c) 2011 Exa Networks. All rights reserved.
-"""
