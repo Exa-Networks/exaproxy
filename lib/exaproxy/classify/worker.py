6dc7bcbcfecd6dae43703bd78f2702d304f2e142
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-02-24 00:41:19 +0000

    RFC notes

--- lib/exaproxy/classify/worker.py
+++ lib/exaproxy/classify/worker.py
@@ -7,6 +7,12 @@ Created by Thomas Mangin on 2011-11-29.
 Copyright (c) 2011 Exa Networks. All rights reserved.
 """
 
+#XXX: RFC VILOATION
+# Need to reply 417 when 100-continue is used (Section 8.2.3)
+# Do not forward 100 response due to Expect:100 if client is HTTP/1.0 (Section 8.2.3)
+# Need to remove connection: header if HTTP is 1/0
+# Not caching so no version update required for us (Section 3.1)
+
 from threading import Thread
 from Queue import Empty
 import subprocess
