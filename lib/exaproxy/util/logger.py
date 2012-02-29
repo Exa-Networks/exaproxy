6ea3ba5c4ebd2d2c2376e6be7e226964a43bf205
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-02-29 13:40:10 +0000

    remvove sheebangs from lib

--- lib/exaproxy/util/logger.py
+++ lib/exaproxy/util/logger.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # encoding: utf-8
 """
 log.py
@@ -7,7 +6,6 @@ Created by Thomas Mangin on 2011-11-29.
 Copyright (c) 2011 Exa Networks. All rights reserved.
 """
 
-#!/usr/bin/env python
 # encoding: utf-8
 """
 utils.py
