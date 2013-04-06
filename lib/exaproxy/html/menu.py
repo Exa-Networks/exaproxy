bab7cf102bb8e72da6cf0141f2f5f9c85031b6c4
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-04-07 00:56:13 +0100

    improving the built-in webserver menu

--- lib/exaproxy/html/menu.py
+++ lib/exaproxy/html/menu.py
@@ -25,12 +25,9 @@ def html (title,header,color='#FF0000',image=png(logo),menu='',text='',):
 				%s
 			</div>
 		</center>
-		<br/>
 		%s
 		<br/>
-		<br/>
 		%s
-		<br/>
 		<br/>
 	</body>
 </html>
@@ -38,133 +35,145 @@ def html (title,header,color='#FF0000',image=png(logo),menu='',text='',):
 """ % (title,color,header,image,menu,text)
 
 
-def _menu (menus,submenus=None):
-		menu_css = """\
-<style type="text/css">
-	.menu {
-		float:left;
-		width:100%;
-		background:#fff;
-		border-bottom:4px solid #000;
-		overflow:hidden;
-		position:relative;
+_menu = """\
+<style type="text/css" media="screen">
+	.droplinebar{
+		overflow: hidden;
 	}
 
-	.menu ul {
-		clear:left;
-		float:left;
-		list-style:none;
-		margin:0;
-		padding:0;
-		position:relative;
-		left:50%;
-		text-align:center;
+	.droplinebar ul{
+		margin: 0;
+		padding: 0;
+		float: left;
+		width: 100%;
+		font: bold 13px Arial;
+		background: #242c54 center center repeat-x; /*default background of menu bar*/
 	}
 
-	.menu ul li {
-		font-family:Arial, Helvetica, sans-serif;
-		font-weight:normal;
-		display:block;
-		float:left;
-		list-style:none;
-		margin:0;
-		padding:0;
-		position:relative;
-		right:50%;
+	.droplinebar ul li{
+		display: inline;
 	}
 
-	.menu ul li a {
-		display:block;
-		margin:0 0 0 1px;
-		padding:3px 10px;
-		background:#aaa;
+	.droplinebar ul li a{
+		float: left;
 		color: white;
-		text-decoration:none;
-		line-height:1.3em;
+		padding: 9px 11px;
+		text-decoration: none;
 	}
 
-	.menu ul li a:visited {
+	.droplinebar ul li a:visited{
+		color: white;
 	}
 
-	.menu ul li a:hover, .menu ul li .current {
-		color: #fff;
-		background-color:#0b75b2;
+	.droplinebar ul li a:hover, .droplinebar ul li .current{ /*background of main menu bar links onMouseover*/
+		color: white;
+	}
+
+	/* Sub level menus*/
+	.droplinebar ul li ul{
+		position: absolute;
+		z-index: 100;
+		left: 0;
+		top: 0;
+		background: #303c76; /*sub menu background color */
+		visibility: hidden;
+	}
+
+	/* Sub level menu links style */
+	.droplinebar ul li ul li a{
+		font: normal 13px Verdana;
+		padding: 6px;
+		padding-right: 8px;
+		margin: 0;
+		border-bottom: 1px solid navy;
+	}
+
+	.droplinebar ul li ul li a:hover{ /*sub menu links' background color onMouseover */
+		background: #242c54;
 	}
 </style>
-"""
 
-		menu = """
-<div class="menu">
-	<ul>
-		%s
-	</ul>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+
+<script type="text/javascript">
+	/*********************
+	//* jQuery Drop Line Menu- By Dynamic Drive: http://www.dynamicdrive.com/
+	//* Last updated: May 9th, 11'
+	//* Menu avaiable at DD CSS Library: http://www.dynamicdrive.com/style/
+	*********************/
+
+	var droplinemenu={
+
+		animateduration: {over: 200, out: 100}, //duration of slide in/ out animation, in milliseconds
+
+		buildmenu:function(menuid){
+			jQuery(document).ready(function($){
+				var $mainmenu=$("#"+menuid+">ul")
+				var $headers=$mainmenu.find("ul").parent()
+				$headers.each(function(i){
+					var $curobj=$(this)
+					var $subul=$(this).find('ul:eq(0)')
+					this._dimensions={h:$curobj.find('a:eq(0)').outerHeight()}
+					this.istopheader=$curobj.parents("ul").length==1? true : false
+					if (!this.istopheader)
+						$subul.css({left:0, top:this._dimensions.h})
+					var $innerheader=$curobj.children('a').eq(0)
+					$innerheader=($innerheader.children().eq(0).is('span'))? $innerheader.children().eq(0) : $innerheader //if header contains inner SPAN, use that
+					$curobj.hover(
+						function(e){
+							var $targetul=$(this).children("ul:eq(0)")
+							if ($targetul.queue().length<=1) //if 1 or less queued animations
+								if (this.istopheader)
+									$targetul.css({left: $mainmenu.position().left, top: $mainmenu.position().top+this._dimensions.h})
+								if (document.all && !window.XMLHttpRequest) //detect IE6 or less, fix issue with overflow
+									$mainmenu.find('ul').css({overflow: (this.istopheader)? 'hidden' : 'visible'})
+								$targetul.dequeue().slideDown(droplinemenu.animateduration.over)
+						},
+						function(e){
+							var $targetul=$(this).children("ul:eq(0)")
+							$targetul.dequeue().slideUp(droplinemenu.animateduration.out)
+						}
+					) //end hover
+				}) //end $headers.each()
+				$mainmenu.find("ul").css({display:'none', visibility:'visible', width:$mainmenu.width()})
+			}) //end document.ready
+		}
+	}
+
+	//build menu with DIV ID="myslidemenu" on page:
+	droplinemenu.buildmenu("mydroplinemenu");
+</script>
+
+<div id="mydroplinemenu" class="droplinebar">
+<ul>
+	<li><a href="/index.html">home</a></li>
+	<li><a href="/information.html">information</a>
+		<ul>
+			<li><a href="/information/introspection/supervisor.html">introspection</a></li>
+			<li><a href="/information/configuration.html">configuration</a></li>
+			<li><a href="/information/statistics.html">statistics</a></li>
+		</ul>
+	</li>
+	<li><a href="/performance.html">performance</a>
+		<ul>
+			<li><a href="/performance/processes.html">processes</a></li>
+			<li><a href="/performance/connections.html">connections</a></li>
+			<li><a href="/performance/clients.html">clients</a></li>
+			<li><a href="/performance/servers.html">servers</a></li>
+			<li><a href="/performance/transfered.html">transfered</a></li>
+			<li><a href="/performance/loops.html">loops</a></li>
+			<li><a href="/performance/events.html">events</a></li>
+		</ul>
+	</li>
+	<li><a href="/performance.html">about</a>
+		<ul>
+			<li><a href="/about/email.html">about</a></li>
+			<li><a href="/about/licence.html">licence</a></li>
+		</ul>
+	</li>
+</ul>
 </div>
-""" % '\n'.join(['<li/><a href="%s">%s</a>' % _ for _ in menus])
-
-		if not submenus:
-			return menu_css + menu
-
-		submenu_css = """\
-<style type="text/css">
-	.submenu {
-		float:left;
-		width:100%;
-		background:#fff;
-		overflow:hidden;
-		position:relative;
-	}
-
-	.submenu ul {
-		clear:left;
-		float:left;
-		list-style:none;
-		margin:0;
-		padding:0;
-		position:relative;
-		left:50%;
-		text-align:center;
-	}
-
-	.submenu ul li {
-		font-family:Arial, Helvetica, sans-serif;
-		font-weight:normal;
-		display:block;
-		float:left;
-		list-style:none;
-		margin:0;
-		padding:0;
-		position:relative;
-		right:50%;
-	}
-
-	.submenu ul li a {
-		display:block;
-		margin:0 0 0 1px;
-		padding:3px 10px;
-		background:#aaa;
-		color: white;
-		text-decoration:none;
-		line-height:1.3em;
-	}
-
-	.submenu ul li a:visited {
-	}
-
-	.submenu ul li a:hover, .submenu ul li .current {
-		color: #fff;
-		background-color:#0b75b2;
-	}
-</style>
 """
-
-		submenu = """
-<div class="submenu">
-	<ul>
-		%s
-	</ul>
-</div>
-""" % '\n'.join(['<li/><a href="%s">%s</a>' % _ for _ in submenus])
-		return menu_css + menu + submenu_css + submenu
 
 _title = 'ExaProxy Monitoring'
 _image = '<a href="http://www.exa-networks.co.uk/" target="exa-networks">%s</a>' % png(logo)
@@ -172,10 +181,10 @@ _image = '<a href="http://www.exa-networks.co.uk/" target="exa-networks">%s</a>'
 
 class Menu (object):
 	def __init__ (self,options,options_information,options_performance,options_about):
-		self._html_root = html(_title,'','#00BB55',_image,_menu(options,None),'*string*').replace('%','%%').replace('*string*','%s')
-		self._html_information = html(_title,'','#00BB55',_image,_menu(options,options_information),'*string*').replace('%','%%').replace('*string*','%s')
-		self._html_performance = html(_title,'','#00BB55',_image,_menu(options,options_performance),'*string*').replace('%','%%').replace('*string*','%s')
-		self._html_about = html(_title,'','#00BB55',_image,_menu(options,options_about),'*string*').replace('%','%%').replace('*string*','%s')
+		self._html_root = html(_title,'','#9999FF',_image,_menu,'*string*').replace('%','%%').replace('*string*','%s')
+		self._html_information = html(_title,'','#9999FF',_image,_menu,'*string*').replace('%','%%').replace('*string*','%s')
+		self._html_performance = html(_title,'','#9999FF',_image,_menu,'*string*').replace('%','%%').replace('*string*','%s')
+		self._html_about = html(_title,'','#9999FF',_image,_menu,'*string*').replace('%','%%').replace('*string*','%s')
 
 	def root (self,page):
 		return self._html_root % page
