9b2c60cbd86243d0365935558697b4aca1e42d3b
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-05-07 22:31:51 +0100

    deal with cpython and pypy implementations

--- lib/exaproxy/util/cache.py
+++ lib/exaproxy/util/cache.py
@@ -4,6 +4,8 @@ from collections import OrderedDict
 from time import time
 
 class TimeCache (dict):
+	__default = object()
+
 	def __init__ (self,timeout):
 		self.timeout = timeout
 		self.last = None
@@ -14,6 +16,19 @@ class TimeCache (dict):
 		dict.__setitem__(self,key,value)
 		if self.timeout > 0:
 			self.time[key] = time()
+
+	def __delitem__ (self,key):
+		if key in self.time:
+			del self.time[key]
+		dict.__delitem__(self,key)
+
+	# Cpython implementation of dict.pop does not call __delitem__ - sigh !
+	def pop (self,key,default=__default):
+		if key in self.time:
+			del self.time[key]
+		if default is self.__default:
+			return dict.pop(self,key)
+		return dict.pop(self,key,default)
 
 	def expired (self,maximum):
 		expire = time() - self.timeout
