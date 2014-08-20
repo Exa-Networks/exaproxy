48d6fbe1465e1170c26cce95dc11883ba75a3749
David Farrar <david.farrar@exa-networks.co.uk>
2014-08-20 17:38:00 +0100

    handle errors reading stats over a closed pipe

--- lib/exaproxy/monitor.py
+++ lib/exaproxy/monitor.py
@@ -24,11 +24,16 @@ class Monitor (object):
 	def zero (self):
 		# make sure we always have data in seconds
 		start = dict()
-		for k in self.statistics():
+		stats = self.statistics()
+
+		for k in stats:
 			start[k] = 0
 
-		self.seconds.append(start)
-		self.minutes.append(start)
+		if start:
+			self.seconds.append(start)
+			self.minutes.append(start)
+
+		return bool(stats)
 
 	def introspection (self,objects):
 		obj = self._container
@@ -119,7 +124,7 @@ class Monitor (object):
 		redirector = self._supervisor.redirector
 		reactor = self._supervisor.reactor
 
-		[redirector_stats] = redirector.getStats()
+		[redirector_stats] = redirector.getStats() or [{}]
 
 		return {
 			'pid.saved' : self._supervisor.pid._saved_pid,
@@ -140,24 +145,24 @@ class Monitor (object):
 			'load.loops' : reactor.nb_loops,
 			'load.events' : reactor.nb_events,
 			'queue.size' : redirector_stats['queue'],
-		}
+		} if redirector_stats else {}
 
 	def second (self):
-		self.seconds.append(self.statistics())
+		stats = self.statistics()
+		if stats:
+			self.seconds.append(stats)
+
 		if len(self.seconds) > self.nb_recorded:
 			self.seconds.popleft()
 
+		return bool(stats)
+
 	def minute (self):
-		self.minutes.append(self.statistics())
+		stats = self.statistics()
+		if stats:
+			self.minutes.append(stats)
+
 		if len(self.minutes) > self.nb_recorded:
 			self.minutes.popleft()
 
-		# minutes = defaultdict(lambda: 0)
-
-		# for record in self.seconds:
-		# 	for k,v in record.items():
-		# 		minutes[k] += v
-
-		# self.minutes.append(minutes)
-		# if len(self.minutes) > self.nb_recorded:
-		# 	self.seconds.popleft()
+		return bool(stats)
